b2bf96bcc7788a3e87a80e90dc5925415df2095a
Bhavesh Singh <arvindbhavesh2004@gmail.com>
2024-10-01 16:28:19 +0530

    Int variable type

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 fun main() {
     var userName: String = "Ness"
     userName = "Bhavesh"
-    println("Hello $userName!")
+
+    var age: Int = 18
+    age = 117
+    println("Hello $userName you are $age years old")
 }
